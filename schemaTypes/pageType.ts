e0e9a0babd6e0a7646184f6af1b54cb043545db1
Jeremy Swinnen <hey@jeremys.be>
2026-02-21 13:32:47 +0100

    feat: add post and page content schemas

---
+++ schemaTypes/pageType.ts
@@ -0,0 +1,25 @@
+import {defineField, defineType} from 'sanity'
+
+export const pageType = defineType({
+  name: 'page',
+  title: 'Page',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {source: 'title'},
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'body',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+  ],
+})
